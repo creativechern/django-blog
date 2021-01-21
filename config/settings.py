3ab172c58bd4cf8fa8368bee276886014bcf91fb
Xuehua Chen <creativechern@outlook.com>
2021-01-20 16:39:58 -0800

    Added STATICFILES_DIRS, STATIC_ROOT and STATICFILES_FINDERS for static file serving.

--- config/settings.py
+++ config/settings.py
@@ -117,5 +117,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+#STATIC_URL = '/static/'
+#STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')),] # new
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
+STATICFILES_FINDERS = [ # new
+   "django.contrib.staticfiles.finders.FileSystemFinder",
+   "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
