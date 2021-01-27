024b087156b3d4fb0a57deac68fcdd14333bd9a7
Xuehua Chen <xuehua@gmail.com>
2021-01-26 17:19:42 -0800

    If DEBUG variable is not set in environment, set it to1.

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG = int(os.environ.get('DEBUG', default=1))
 
 #ALLOWED_HOSTS = []
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'young-dawn-09814.herokuapp.com']
