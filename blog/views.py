0bbd0ed234460f8d009fbeba9e556f2ddc225820
Xuehua Chen <xuehua@gmail.com>
2021-02-05 16:30:08 -0800

    Use list view to display multiple posts.

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Post
 from .forms import PostForm
-class HomepageView(TemplateView):
-    template_name = "blog/index.html"
 
+class BlogListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
 
 class AboutView(TemplateView):
     template_name = "blog/about.html"
-
 
 class ContactView(TemplateView):
     template_name = "blog/contact.html"
