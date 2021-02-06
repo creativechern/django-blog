05b82a649ab239040cb3c9124d52e8ecb049027a
Xuehua Chen <xuehua@gmail.com>
2021-02-05 21:01:04 -0800

    Minor change to display blogs.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.HomepageView.as_view(), name="home"),
+    path('', views.BlogListView.as_view(), name="home"),
     path('about/', views.AboutView.as_view(), name="about"),
     path('contact/', views.ContactView.as_view(), name="contact"),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name="post_detail"),
