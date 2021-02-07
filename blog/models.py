7d08dec77f9d99359a803a0fcacec88a8f29be17
Xuehua Chen <xuehua@gmail.com>
2021-02-06 19:26:09 -0800

    Add author field to the post form.

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
 class Post(models.Model):
 
     title = models.CharField(max_length = 250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete = models.CASCADE,
+    )
     body = models.TextField()
 
     def __str__(self):
